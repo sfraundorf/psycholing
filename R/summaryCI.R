ce2766568765c630063853a33088f93dd8530d4e
Scott Fraundorf <scottfraundorf@gmail.com>
2020-04-08 16:25:31 -0400

    Add digits argument to summaryCI
    Add argument to summaryCI to specify how many digits to round to

--- R/summaryCI.R
+++ R/summaryCI.R
@@ -8,6 +8,7 @@
 #'
 #' @param model \code{lmer} model to be summarized.
 #' @param confidence desired level of confidence for the confidence interval.
+#' @param digits integer, used for number formatting
 #' @return model summary including confidence intervals.
 #' @references Baayen, R.H. (2008). \emph{Analyzing linguistic data: A practical
 #' introduction to statistics}. Cambridge, UK: Cambridge University Press.
@@ -18,7 +19,7 @@
 #' @export
 #' @importFrom stats family qnorm
 
-summaryCI <- function(model, confidence=.95) {
+summaryCI <- function(model, confidence=.95, digits=3) {
 
 	# First, check that this was a GAUSSIAN model:
 	if (family(model)$family != "gaussian") {
@@ -44,11 +45,11 @@ summaryCI <- function(model, confidence=.95) {
 	ci.upper <- coefs[,1] + (quantile * coefs[,2])
 
 	# Add these to the coefficients table:
-	model.summary$coefficients <- cbind('Estimate'=coefs[,'Estimate'],
-	                                    'Std. Error'=coefs[,'Std. Error'],
-	                                    't value'=coefs[,'t value'],
-	                                    'Lower CI'=ci.lower,
-	                                    'Upper CI'=ci.upper)
+	model.summary$coefficients <- cbind('Estimate'=round(coefs[,'Estimate'], digits),
+	                                    'Std. Error'=round(coefs[,'Std. Error'], digits),
+	                                    't value'=round(coefs[,'t value'], digits),
+	                                    'Lower CI'=round(ci.lower, digits),
+	                                    'Upper CI'=round(ci.upper, digits))
 
 	# Display:
 	model.summary
